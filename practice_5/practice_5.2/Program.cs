5b50356cfbc8f44c612c398e4fa113a91c455811
Dorrothee <112761090+Dorrothee@users.noreply.github.com>
2022-10-26 00:01:09 +0300

    Add files via upload

---
+++ practice_5/practice_5.2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Diagnostics;
+
+
+class PBKDF2_Hash
+{
+    static void Main()
+    {
+        do
+        {
+            Console.WriteLine("Want you to run program?" + "   YES / NO");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+            if (choice.ToLower()=="yes")
+            {
+                Console.WriteLine("Choose which method for hashing you want to use:");
+                Console.WriteLine("1 - SHA1");
+                Console.WriteLine("2 - SHA256");
+                Console.WriteLine("3 - SHA384");
+                Console.WriteLine("4 - SHA512");
+                Console.WriteLine("5 - MD5");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("Enter the password you want to hash:");
+                string password = null;
+                while (true)
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Enter)
+                        break;
+                    password += key.KeyChar;
+                }
+                int iteration = 40000;
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                HashAlgorithmName hashAlgorithmName;
+                switch (option)
+                {
+                    case 1:
+                        hashAlgorithmName = HashAlgorithmName.SHA1;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            PBKDF2.HashPassword(password, iteration, hashAlgorithmName);
+                            iteration += 50000;
+                        }
+                        break;
+                    case 2:
+                        hashAlgorithmName = HashAlgorithmName.SHA256;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            PBKDF2.HashPassword(password, iteration, hashAlgorithmName);
+                            iteration = iteration + 50000;
+                        }
+                        break;
+                    case 3:
+                        hashAlgorithmName = HashAlgorithmName.SHA384;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            PBKDF2.HashPassword(password, iteration, hashAlgorithmName);
+                            iteration = iteration + 50000;
+                        }
+                        break;
+                    case 4:
+                        hashAlgorithmName = HashAlgorithmName.SHA512;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            PBKDF2.HashPassword(password, iteration, hashAlgorithmName);
+                            iteration = iteration + 50000;
+                        }
+                        break;
+                    case 5:
+                        hashAlgorithmName = HashAlgorithmName.MD5;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            PBKDF2.HashPassword(password, iteration, hashAlgorithmName);
+                            iteration = iteration + 50000;
+                        }
+                        break;
+                }
+            }
+            if (choice.ToLower() == "no")
+            {
+                break;
+            }
+            
+        }
+        while (true);
+    }
+}
+
+public class PBKDF2
+{
+    public static byte[] GenerateSalt()
+    {
+        const int salt_length = 32;
+        using (var randomNumberGenerator = RandomNumberGenerator.Create())
+        {
+            var rnd = new byte[salt_length];
+            randomNumberGenerator.GetBytes(rnd);
+
+            return rnd;
+        }
+    }
+
+    public static void HashPassword(string passwordToHash, int numOfRounds, HashAlgorithmName hashalgorithmname)
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        var hashedPassword = PBKDF2.HashPasswordhash(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numOfRounds, hashalgorithmname);
+        sw.Stop();
+        Console.WriteLine("Password to hash:" + passwordToHash);
+        Console.WriteLine("Hashed Password via " + hashalgorithmname + " :" + Convert.ToBase64String(hashedPassword));
+        Console.WriteLine("Iterations <" + numOfRounds + "> Elapsed Time: " + sw.ElapsedMilliseconds + "ms");
+        Console.WriteLine();
+    }
+
+    public static byte[] HashPasswordhash(byte[] toBeHashed, byte[] salt, int numOfRounds, HashAlgorithmName hashalgorithm)
+    {
+        using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numOfRounds, hashalgorithm))
+        {
+            int size = 0;
+            if (hashalgorithm == HashAlgorithmName.SHA1)
+            {
+                size = 20;
+            }
+            if (hashalgorithm == HashAlgorithmName.SHA256)
+            {
+                size = 32;
+            }
+            if (hashalgorithm == HashAlgorithmName.SHA384)
+            {
+                size = 48;
+            }
+            if (hashalgorithm == HashAlgorithmName.SHA512)
+            {
+                size = 64;
+            }
+            if (hashalgorithm == HashAlgorithmName.MD5)
+            {
+                size = 16;
+            }
+
+            return rfc2898.GetBytes(size);
+        }
+    }
+}
---
+++ practice_5/practice_5.3/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+class HashProgram
+{
+    public static void Main()
+    {
+        int j = 0;
+        string[] data_login = new string[10];
+        string[] data_password = new string[10];
+        string[] data_salt = new string[10];
+        do
+        {
+            Console.WriteLine("Choose your option:");
+            Console.WriteLine("1 - Create your personal office");
+            Console.WriteLine("2 - To verify your data");
+            Console.WriteLine("3 - Exit");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("\n-----------------------------");
+                    Console.WriteLine("Authentication");
+                    Console.WriteLine("-----------------------------\n\n");
+                    Console.WriteLine("Enter your login:");
+                    string login_input = Console.ReadLine();
+                    byte[] login_repeat = Encoding.UTF8.GetBytes(login_input);
+                    Console.WriteLine("Enter your password:");
+                    string pas_input = null;
+                    while (true)
+                    {
+                        var key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.Enter)
+                            break;
+                        pas_input += key.KeyChar;
+                    }
+                    Console.WriteLine("\n...");
+                    Console.WriteLine("Processing");
+                    Console.WriteLine("...\n");
+                    byte[] login_byte = Encoding.Unicode.GetBytes(login_input);
+                    byte[] salt = GenerateSalt();
+                    string SALT = Convert.ToBase64String(salt);
+                    string hashing = "";
+                    int iteration = 40000;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        hashing = HashingPassword(pas_input, SALT, iteration);
+                        iteration += 50000;
+                    }
+                    data_login[j] = Convert.ToBase64String(login_byte);
+                    data_password[j] = hashing;
+                    data_salt[j] = SALT;
+                    j++;
+                    Console.WriteLine("\n\n-----------------------------");
+                    Console.WriteLine("Done");
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("\n\n-----------------------------");
+                    Console.WriteLine("Verification");
+                    Console.WriteLine("\n\n-----------------------------");
+                    Console.WriteLine("Enter your login:");
+                    string login_verify = Console.ReadLine();
+                    Console.WriteLine("Enter the password:");
+                    string pas_verify = null;
+                    while (true)
+                    {
+                        var key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.Enter)
+                            break;
+                        pas_verify += key.KeyChar;
+                    }
+                    byte[] login_verify_byte = Encoding.Unicode.GetBytes(login_verify);
+
+                    int index_login = Array.IndexOf(data_login, Convert.ToBase64String(login_verify_byte));
+                    string salt_verify = data_salt[index_login];
+                    string hashing_verify = "";
+                    int iteration_verify = 40000;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        hashing_verify = HashingPassword(pas_verify, salt_verify, iteration_verify);
+                        iteration_verify += 50000;
+                    }
+                    if (data_password.Contains(hashing_verify))
+                    {
+                        Console.WriteLine("\n\n-----------------------------");
+                        Console.WriteLine("Such user exists\n\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\n-----------------------------");
+                        Console.WriteLine("Such user does not exist\n\n");
+                    }
+                    break;
+                    Console.ReadLine();
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Choose an option from the list above");
+                    break;
+            }
+        }
+        while (true);
+    }
+
+    public static byte[] GenerateSalt()
+    {
+        const int salt_length = 32;
+        using (var randomNumberGenerator = RandomNumberGenerator.Create())
+        {
+            var rnd = new byte[salt_length];
+            randomNumberGenerator.GetBytes(rnd);
+
+            return rnd;
+        }
+    }
+
+    private static string HashingPassword(string passwordToHash, string salt, int numOfRounds)
+    {
+        byte[] salt_byte = Encoding.UTF8.GetBytes(salt);
+        var hashedPassword = HashAuthentication.HashPassword(Encoding.UTF8.GetBytes(passwordToHash), salt_byte, numOfRounds);
+        return Convert.ToBase64String(hashedPassword);
+    }
+
+}
+
+public class HashAuthentication
+{
+    public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numOfRounds)
+    {
+        using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numOfRounds))
+        {
+            return rfc2898.GetBytes(20);
+        }
+    }
+}
